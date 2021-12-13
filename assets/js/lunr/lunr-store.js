a91b34240959ace7d00ce43a35430983b56c0a7a
Morozzzko <igor@morozov.is>
2021-12-13 00:12:29 +0000

    Deploying to main from @ Morozzzko/Morozzzko.github.io@63993c39efc0fbd3b2abb1e6094bb9a95ff63d6e 🚀

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,40 +1,4 @@
 var store = [{
-        "title": "Railway Oriented programming in Ruby: do notation vs dry-transaction",
-        "excerpt":"Railway oriented programming is a design pattern which helps us handle errors in our applications. Instead of relying on exceptions, we design our data and functions in a specific way. Since applications are essentially just a combination of steps, we’ll make some design decisions about those steps and their structure:...","categories": [],
-        "tags": [],
-        "url": "/2018/05/27/do-notation-ruby.html",
-        "teaser": null
-      },{
-        "title": "Monad laws in Ruby",
-        "excerpt":"I’ve been using monads in Ruby since May 2016, but I haven’t really understood the theoretical basis for them. I thought about learning Haskell, but I gave up pretty soon: I didn’t think I would benefit from it. Moreover, we started using ReasonML in Planado, which improved my functional programming...","categories": [],
-        "tags": [],
-        "url": "/2018/09/08/monad-laws-in-ruby.html",
-        "teaser": null
-      },{
-        "title": "Partial application in Ruby",
-        "excerpt":"Ruby is a multi-paradigm language with a strong bias towards object-oriented programming. You can argue that its design is influenced by Alan Kay and Smalltalk, as opposed to C++/Java-style object-oriented languages. Thankfully, this object-oriented design doesn’t mean we can’t use ideas from functional programming. There’s a small list of functional...","categories": [],
-        "tags": [],
-        "url": "/2019/01/11/partial-application-in-ruby.html",
-        "teaser": null
-      },{
-        "title": "dry-rb 1.0: upgrading validations, types and schemas",
-        "excerpt":"I’m enthusiastic about dry-rb gems. Actually, I’ve never worked on Ruby projects without a dry-rb gem. However, some people are sceptical, as a lot of core dry-rb gems are still in their 0.x phase, which leads to a lot of breaking changes and hours of refactoring. I’m happy to see...","categories": [],
-        "tags": [],
-        "url": "/2019/05/31/upgrading-dry-gems.html",
-        "teaser": null
-      },{
-        "title": "Should I _really_ use monads?",
-        "excerpt":"A couple of weeks ago I witnessed a dialogue in a Ruby chat. I’m paraphrasing, but it went like this: xxx: What is dry? I’ve seen this gem prefix and discussions, but never actually learned about it. yyy: It’s a set of libraries to tackle some problems. zzz: Yeah, and...","categories": [],
-        "tags": [],
-        "url": "/2020/04/01/should-i-really-use-monads.html",
-        "teaser": null
-      },{
-        "title": "Designing helpful service objects. Part 1. Choosing the right design",
-        "excerpt":"I’ve been programming for a long time and I’ve had countless arguments about different things. I’d like to list top four reasons I’ve had an argument online. Style guide. Thankfully, the number of arguments reduces as I mature, but I’m still spending a lot of time on them. I’d rather...","categories": [],
-        "tags": [],
-        "url": "/2020/06/01/helpful-service-objects-part-1-chosing-right-design.html",
-        "teaser": null
-      },{
         "title": "Ducks and Monads: Wonders of Ruby types",
         "excerpt":"Ducks and Monads Wonders of Ruby Types Hey, I'm Igor /'iːgɔːɹ/ I'm a polyglot programmer at Qlean.ru I don't trust myself There are four reasons I don't trust myself undefined is not a function N&nbsp;+&nbsp;1 I mess up business logic I forget to close a bracket I forget some details...","categories": [],
         "tags": [],
